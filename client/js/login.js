1e96162c562a789b22fac846798dc56aaa578ce7
cf9874 <cf9874@gmail.com>
2023-02-04 06:09:38 +0900

    [로그인] refactor : 코드 리팩토링 및 비밀번호 암/복호화 이벤트 세분화

--- client/js/login.js
+++ client/js/login.js
@@ -1,36 +1,41 @@
-import { getNode, toggleClass } from '../lib/index.js';
+import { getNode, toggleClass } from "../lib/index.js";
 
-const autoLogin = getNode('.auto-login-btn');
-const password = getNode('.pw-see');
+const autoLogin = getNode(".auto-login-btn");
+const password = getNode(".pw-see");
+const type = getNode(".pw-type");
 
 function auto_login() {
-  toggleClass(autoLogin, 'clicked');
-  const image = getNode('.auto-login-img');
+  toggleClass(autoLogin, "clicked");
+  const image = getNode(".auto-login-img");
 
-  if (image.src.includes('ed')) {
-    image.src = './assets/icons/login_check_26_26.png';
+  if (image.src.includes("ed")) {
+    image.src = "./assets/icons/login_check_26_26.png";
   } else {
-    image.src = './assets/icons/login_checked_26_26.png';
+    image.src = "./assets/icons/login_checked_26_26.png";
   }
 }
-
-autoLogin.addEventListener('click', auto_login);
-
-function pw_see() {
-  const type = getNode('.pw-type');
-  if (type.type.includes('pass')) {
-    type.type = 'text';
+//마우스 버튼을 눌렀을 때
+function pw_see_mousedown() {
+  if (type.type.includes("pass")) {
+    type.type = "text";
   } else {
-    type.type = 'password';
+    type.type = "password";
   }
-
-  if (password.src.includes('no')) {
-    password.src = './assets/icons/login_see_36_36.svg';
-    password.alt="비밀번호 복호화";
-  } else {
-    password.src = './assets/icons/login_nosee_36_36.png';
-    password.alt="비밀번호 암호화";
-  }
+  password.src = "./assets/icons/login_see_36_36.svg";
+  password.alt = "비밀번호 복호화";
 }
 
-password.addEventListener('click', pw_see);
+//마우스 버튼을 떼었을 때
+function pw_see_mouseup() {
+  if (type.type.includes("pass")) {
+    type.type = "text";
+  } else {
+    type.type = "password";
+  }
+  password.src = "./assets/icons/login_nosee_36_36.png";
+  password.alt = "비밀번호 암호화";
+}
+
+autoLogin.addEventListener("click", auto_login);
+password.addEventListener("mousedown", pw_see_mousedown);
+password.addEventListener("mouseup", pw_see_mouseup);
