6093b9367df144f7948613035df2f79d8b27a4b9
cf9874 <cf9874@gmail.com>
2023-02-04 05:28:06 +0900

    [메인] feat : 모달창 오늘하루 보지않기, 닫기 버튼 기능 구현 (#147, #149)

--- client/js/main.js
+++ client/js/main.js
@@ -1,3 +1,23 @@
 import createSwiper from "./createSwiper.js";
+import { getNode, saveStorage, loadStorage } from "../lib/index.js";
 
 createSwiper();
+
+const main = getNode("main");
+const mainModal = getNode(".main-event-modal");
+const mainModalCloseDefaultButton = getNode(".main-evnent-modal-close li:last-child button");
+const mainModalCloseTodayButton = getNode(".main-evnent-modal-close li:first-child button");
+
+let isModalCloseToday = await loadStorage("close_today");
+if (isModalCloseToday) {
+  main.removeChild(mainModal);
+}
+
+mainModalCloseDefaultButton.addEventListener("click", () => {
+  mainModal.style.display = "none";
+});
+
+mainModalCloseTodayButton.addEventListener("click", () => {
+  saveStorage("close_today", "true");
+  main.removeChild(mainModal);
+});
