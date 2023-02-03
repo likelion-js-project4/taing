f661a4d793fe4b9c2ffba95d5c5722a8ddbcaf9d
cf9874 <cf9874@gmail.com>
2023-02-04 06:55:45 +0900

    [로그인] feat : 로그인 기능 임시 구현
    routing 기능 추가 필요

--- client/js/login.js
+++ client/js/login.js
@@ -1,8 +1,58 @@
-import { getNode, toggleClass } from "../lib/index.js";
+import { getNode, toggleClass, getInputValue, tiger, saveStorage } from "../lib/index.js";
 
 const autoLogin = getNode(".auto-login-btn");
 const password = getNode(".pw-see");
 const type = getNode(".pw-type");
+const inputId = getNode("#login-id");
+const inputPw = getNode("#login-pw");
+const loginButton = getNode(".btn-login");
+
+function idCheck(db, id) {
+  const user = db.filter((e) => e.email === id);
+  console.log(1212, user);
+  if ((user.length === 0) & (user.email !== id)) {
+    alert("가입되지 않은 회원입니다.");
+  }
+  return;
+}
+function pwCheck(db, pw) {
+  const user = db.filter((e) => e.password === pw);
+  console.log(2020, user);
+  if ((user.length === 0) & (user.password !== pw)) {
+    alert("비밀번호 틀렸음");
+  }
+  return;
+}
+
+async function userCheck(idNode, pwNode) {
+  const idValue = getInputValue(idNode);
+  const pwValue = getInputValue(pwNode);
+
+  const userData = await tiger.get("http://localhost:3000/users");
+  const { data } = userData;
+  const user = data.filter((e) => e.email === idValue);
+
+  if (user.length === 0) {
+    alert("입력하신 아이디와 일치하는 회원이 존재하지 않습니다.");
+    return;
+  }
+
+  if (user[0].password !== pwValue) {
+    alert("비밀번호가 일치하지 않습니다.");
+    return;
+  }
+  return true;
+}
+
+async function login(idNode, pwNode) {
+  const isUser = userCheck(idNode, pwNode);
+
+  if (isUser) {
+    //로그인 성공 후 localStorage에 저장
+    saveStorage("userLogin", "true");
+    //routing 처리
+  }
+}
 
 function auto_login() {
   toggleClass(autoLogin, "clicked");
@@ -39,3 +89,4 @@ function pw_see_mouseup() {
 autoLogin.addEventListener("click", auto_login);
 password.addEventListener("mousedown", pw_see_mousedown);
 password.addEventListener("mouseup", pw_see_mouseup);
+loginButton.addEventListener("click", () => login(inputId, inputPw));
