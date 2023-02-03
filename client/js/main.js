18702d184b80f53786168939f143c7ac73d8e724
cf9874 <cf9874@gmail.com>
2023-02-04 07:17:13 +0900

    [헤더] feat : 실시간 메뉴 hover시 이미지 소스 변경

--- client/js/main.js
+++ client/js/main.js
@@ -4,6 +4,7 @@ import { getNode, saveStorage, loadStorage } from "../lib/index.js";
 createSwiper();
 
 const main = getNode("main");
+const header = getNode(".header-alive");
 const mainModal = getNode(".main-event-modal");
 const mainModalCloseDefaultButton = getNode(".main-evnent-modal-close li:last-child button");
 const mainModalCloseTodayButton = getNode(".main-evnent-modal-close li:first-child button");
@@ -12,6 +13,13 @@ let isModalCloseToday = await loadStorage("close_today");
 if (isModalCloseToday) {
   main.removeChild(mainModal);
 }
+
+header.addEventListener("mouseenter", () => {
+  header.children[0].src = "./assets/icons/header_live_active_34_34.png";
+});
+header.addEventListener("mouseleave", () => {
+  header.children[0].src = "./assets/icons/header_live_default_34_34.png";
+});
 
 mainModalCloseDefaultButton.addEventListener("click", () => {
   mainModal.style.display = "none";
