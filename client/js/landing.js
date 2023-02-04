cdc06efd850754a6f05070970bce65795741acf8
Yesong Choi <to06109@naver.com>
2023-02-05 04:12:50 +0900

    [랜딩페이지] design: 마크업 및 반응형 리뷰 1차 수정

--- client/js/landing.js
+++ client/js/landing.js
@@ -18,3 +18,58 @@ window.addEventListener('DOMContentLoaded', function () {
       item.querySelector('ul').offsetWidth + 'px'
   })
 })
+
+const landingSwiper = new Swiper('.taing-only-contents .swiper', {
+  autoPlay: true,
+  slidesPerView: 2.2,
+  spaceBetween: 10,
+  centeredSlides: true,
+  // slidesPerGroup: 2,
+  mousewheel: true,
+  on: {
+    reachEnd: function () {
+      landingSwiper.mousewheel.disable()
+    },
+    // reachBegeinning: function () {
+    //   landingSwiper.mousewheel.disable()
+    // },
+  },
+  // breakpoints: {
+  //   768: {
+  //     slidesPerView: 3,
+  //     slidesPerGroup: 3,
+  //   },
+  //   1280: {
+  //     slidesPerView: 5,
+  //     slidesPerGroup: 5,
+  //     spaceBetween: 16,
+  //   },
+  // },
+})
+
+// on: {
+//   slideChange: function() {
+//       setTimeout(function () {
+//           visual.params.touchReleaseOnEdges = false;
+//           visual.params.mousewheel.releaseOnEdges = false;
+//       });
+//   },
+//   reachEnd: function() {
+//       setTimeout(function () {
+//           visual.params.touchReleaseOnEdges = true;
+//           visual.params.mousewheel.releaseOnEdges = true;
+//       }, 500);
+//   },
+//   reachBeginning: function() {
+//       setTimeout(function () {
+//           visual.params.touchReleaseOnEdges = true;
+//           visual.params.mousewheel.releaseOnEdges = true;
+//       }, 500);
+//   }
+// }
+
+window.addEventListener('wheel', function (event) {
+  if (event.deltaY < 0) {
+    landingSwiper.mousewheel.enable()
+  }
+})
