776ca8415ea438532065d80401c299bf27217785
semin99 <jsm7494@naver.com>
2023-02-05 18:16:04 +0900

    [랜딩] test : gsap만 이용한 스와이프 js 삽입

--- client/js/landing.js
+++ client/js/landing.js
@@ -6,64 +6,19 @@ import {
 
 gsap.registerPlugin(ScrollTrigger);
 
-const pageContainer = $(".container")
+let sections = gsap.utils.toArray(".landing-img-box");
 
-/* SMOOTH SCROLL */
-const scroller = new LocomotiveScroll({
-  el: pageContainer,
-  smooth: true
+gsap.to(sections, {
+  xPercent: -100 * (sections.length - 1),
+  ease: "none",
+  scrollTrigger: {
+    trigger: "#sectionPin",
+    pin: true,
+    scrub: 1,
+    // snap: 1 / (sections.length - 1),
+    end: () => "+=" + $("#sectionPin").offsetWidth
+  }
 });
-
-function scrollContentHandler() {
-  
-scroller.on("scroll", ScrollTrigger.update);
-
-  ScrollTrigger.scrollerProxy(pageContainer, {
-    scrollTop(value) {
-      return arguments.length
-        ? scroller.scrollTo(value, 0, 0)
-        : scroller.scroll.instance.scroll.y;
-    },
-    getBoundingClientRect() {
-      return {
-        left: 0,
-        top: 0,
-        width: window.innerWidth,
-        height: window.innerHeight
-      };
-    },
-    pinType: pageContainer.style.transform ? "transform" : "fixed"
-  });
-
-  let pinBoxes = document.querySelectorAll(".pin-wrap > *")
-  let pinWrap = document.querySelector(".pin-wrap")
-  let pinWrapWidth = pinWrap.offsetWidth
-  let horizontalScrollLength = pinWrapWidth - window.innerWidth
-  
-// Pinning and horizontal scrolling
-
-  gsap.to(".pin-wrap", {
-    scrollTrigger: {
-      scroller: pageContainer, //locomotive-scroll
-      scrub: true,
-      trigger: "#sectionPin",
-      pin: true,
-      // anticipatePin: 1,
-      start: "top top",
-      end: pinWrapWidth
-    },
-    x: -horizontalScrollLength,
-    ease: "none"
-  });
-
-  ScrollTrigger.addEventListener("refresh", () => scroller.update()); //locomotive-scroll
-
-  ScrollTrigger.refresh();
-}
-
-window.addEventListener("load", scrollContentHandler);
-
-// window.addEventListener("resize", scrollContentHandler);
 
 function appendClone(node, clone) {
   let wrap = node.closest('.wrap')
