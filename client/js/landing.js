f0a9245433ca2ea9d3bec9e8a59cdf47627df0ba
semin99 <jsm7494@naver.com>
2023-02-05 17:20:16 +0900

    [메인] test : 스크롤 반응 스와이프 기능 테스트

--- client/js/landing.js
+++ client/js/landing.js
@@ -1,3 +1,70 @@
+/* global gsap */
+
+import { 
+  getNode as $,
+} from "../lib/index.js";
+
+gsap.registerPlugin(ScrollTrigger);
+
+const pageContainer = $(".container")
+
+/* SMOOTH SCROLL */
+const scroller = new LocomotiveScroll({
+  el: pageContainer,
+  smooth: true
+});
+
+function scrollContentHandler() {
+  
+scroller.on("scroll", ScrollTrigger.update);
+
+  ScrollTrigger.scrollerProxy(pageContainer, {
+    scrollTop(value) {
+      return arguments.length
+        ? scroller.scrollTo(value, 0, 0)
+        : scroller.scroll.instance.scroll.y;
+    },
+    getBoundingClientRect() {
+      return {
+        left: 0,
+        top: 0,
+        width: window.innerWidth,
+        height: window.innerHeight
+      };
+    },
+    pinType: pageContainer.style.transform ? "transform" : "fixed"
+  });
+
+  let pinBoxes = document.querySelectorAll(".pin-wrap > *")
+  let pinWrap = document.querySelector(".pin-wrap")
+  let pinWrapWidth = pinWrap.offsetWidth
+  let horizontalScrollLength = pinWrapWidth - window.innerWidth
+  
+// Pinning and horizontal scrolling
+
+  gsap.to(".pin-wrap", {
+    scrollTrigger: {
+      scroller: pageContainer, //locomotive-scroll
+      scrub: true,
+      trigger: "#sectionPin",
+      pin: true,
+      // anticipatePin: 1,
+      start: "top top",
+      end: pinWrapWidth
+    },
+    x: -horizontalScrollLength,
+    ease: "none"
+  });
+
+  ScrollTrigger.addEventListener("refresh", () => scroller.update()); //locomotive-scroll
+
+  ScrollTrigger.refresh();
+}
+
+window.addEventListener("load", scrollContentHandler);
+
+// window.addEventListener("resize", scrollContentHandler);
+
 function appendClone(node, clone) {
   let wrap = node.closest('.wrap')
   wrap.appendChild(clone)
@@ -18,45 +85,3 @@ window.addEventListener('DOMContentLoaded', function () {
       item.querySelector('ul').offsetWidth + 'px'
   })
 })
-
-const landingSwiper = new Swiper('.taing-only-contents .swiper', {
-  autoPlay: false,
-  spaceBetween: 15,
-  centeredSlides: true,
-  mousewheel: true,
-  on: {
-    slideChange: function () {
-      setTimeout(function () {
-        landingSwiper.params.touchReleaseOnEdges = false
-        landingSwiper.params.mousewheel.releaseOnEdges = false
-      })
-    },
-    reachEnd: function () {
-      setTimeout(function () {
-        landingSwiper.params.touchReleaseOnEdges = true
-        landingSwiper.params.mousewheel.releaseOnEdges = true
-      }, 500)
-    },
-    reachBeginning: function () {
-      setTimeout(function () {
-        landingSwiper.params.touchReleaseOnEdges = true
-        landingSwiper.params.mousewheel.releaseOnEdges = true
-      }, 500)
-    },
-  },
-  breakpoints: {
-    320: {
-      slidesPerView: 1.2,
-    },
-    768: {
-      slidesPerView: 2.7,
-      spaceBetween: 10,
-    },
-    1280: {
-      slidesPerView: 2.1,
-      spaceBetween: 30,
-    },
-  },
-})
-
-landingSwiper()
