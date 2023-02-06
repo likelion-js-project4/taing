ae1b034b2a3942fda2967e011bdf694ddae2f4da
yoonho <dydgh179@gmail.com>
2023-02-06 21:56:41 +0900

    [회원가입] refactor : 모두동의합니 코드수정

--- client/js/register.js
+++ client/js/register.js
@@ -1,4 +1,4 @@
-import { getNode, getNodes} from '../lib/index.js';
+import { getNode, getNodes, toggleClass} from '../lib/index.js';
 
 const inputId = getNode('#register-id');
 const inputPassword = getNode('#register-pw');
@@ -10,35 +10,27 @@ const passwordCancelButton= getNode('.pw-cancel-btn');
 const pwCheckCancelButton= getNode('.pwcheck-cancel-btn');
 const emailCancelButton= getNode('.email-cancel-btn');
 
-const pw_see_button = getNode('.pw-see-btn');
-const pwcheck_see_button = getNode('.pw-see2-btn');
+const pwSeeButton = getNode('.pw-see-btn');
+const pwCheckSeeButton = getNode('.pw-see2-btn');
 
 const idContent = getNode('.register-id-content');
 const pwContent = getNode('.register-pw-content')
 
-// const register_list= getNodes('input[name=register]');
+const registerWrapper = getNodes('.register-content-wrapper'); 
+
 const checkAll= getNode('#register-all');   //모두동의합니다 input
-const check1 = getNode('#register-item1');
-const check2 = getNode('#register-item2');
-const check3 = getNode('#register-item3');
-const check4 = getNode('#register-item4');
-const check5 = getNode('#register-item5');
-const check6 = getNode('#register-item6');
-const check7 = getNode('#register-item7');
-const check8 = getNode('#register-item8');
+const checkBoxes = getNodes('.register-content-item > input');  //전체 체크박스
+const checkedAll = getNodes('.input[name="register"]:checked');  //선택된 체크박스들
+// const check1 = getNode('#register-item1');
+// const check2 = getNode('#register-item2');
+// const check3 = getNode('#register-item3');
+// const check4 = getNode('#register-item4');
+// const check5 = getNode('#register-item5');
+// const check6 = getNode('#register-item6');
+// const check7 = getNode('#register-item7');
+// const check8 = getNode('#register-item8');
 
 const submitButton  = getNode('.btn-register');     //최종 가입하기버튼
-
-//회원가입 하기전 요건
-const agreements={
-  id : false,
-  pw : false,
-  email : false,
-  check1 : false,
-  check2 : false,
-  check3 : false,
-  check4 : false,
-};
 
 // input 함수
 function cancel_active(node){
@@ -96,32 +88,29 @@ function pw_see(node){
   }
 }
 
-//모두 동의하기 버튼
-function all_check(){
-  const checkboxes = getNodes('.register-content-item > input');
+//모두동의합니다 누르면 모두 체크
+checkAll.addEventListener('click', () => {
+  checkBoxes.forEach(item => {
+    item.checked = checkAll.checked;
+  })
+})
 
-  for (let checkbox of checkboxes){
-    checkbox.checked = this.checked;
-  }
-}
-
-// 모두동의버튼 안누르고 모든 체크를 하면 동의하기버튼도 활성화
-// if (check1 && check2 && check3 && check4 %% check5 && check6 && check7 && check8) {
-//   checkAll.checked = true;
-// } else {
-//   checkAll.checked = false;
-// }
-
-//모든 회원가입 조건을 만족시켰을시 버튼 활성화시키기
-// for(let key in agreements) {
-//   if(key ==false){
-//     return false;
-//   }else{
-//     submitButton.disabled=false;
-//   }
-// }
-
-
+//모두 클릭되면 (모두 동의합니다) 도 체크되기
+checkBoxes.forEach(item => {
+  item.addEventListener("click", ()=>{
+    let count = 0;
+    checkBoxes.forEach(item => {
+      if(item.checked){
+        count ++;
+      }
+    })
+    if(count == checkBoxes.length){
+      checkAll.checked = true;
+    }else{
+      checkAll.checked = false;
+    }
+  })
+})
 
 inputId.addEventListener('keyup', () => cancel_active(inputId));
 inputPassword.addEventListener('keyup', () => cancel_active(inputPassword));
@@ -133,8 +122,15 @@ passwordCancelButton.addEventListener('click', ()=> delete_input(inputPassword))
 pwCheckCancelButton.addEventListener('click', ()=> delete_input(inputPasswordcheck));
 emailCancelButton.addEventListener('click', ()=> delete_input(inputEmail));
 
-pw_see_button.addEventListener('click', () => pw_see(pw_see_button));
-pwcheck_see_button.addEventListener('click', () => pw_see(pwcheck_see_button));
+pwSeeButton.addEventListener('click', () => pw_see(pwSeeButton));
+pwCheckSeeButton.addEventListener('click', () => pw_see(pwCheckSeeButton));
 
-checkAll.addEventListener('click', all_check);
+
+//function all_check(){
+  //   const checkboxes = getNodes('.register-content-item > input');
+  
+  //   for (let checkbox of checkboxes){
+  //     checkbox.checked = this.checked;
+  //   }
+  // }
 
