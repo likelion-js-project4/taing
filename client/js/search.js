d57875adb4527f9c4f8e0de385063c81f9f000ab
Yesong Choi <to06109@naver.com>
2023-02-06 21:54:39 +0900

    [검색] feat: 최근 검색어 업데이트 기능 구현 #52

--- client/js/search.js
+++ client/js/search.js
@@ -1,18 +1,30 @@
-import { getNode, css } from '../lib/index.js'
+import { getNode, css, tiger, saveStorage, loadStorage } from '../lib/index.js'
+
+const SEARCH_KEY = 'taing_search'
 
 const searchInput = getNode('.search-form-input')
 const searchForm = getNode('.search-form')
 const alert = getNode('.alert-section')
 const alertButton = getNode('.enroll-btn')
-console.log(searchInput)
+
+function updateSearch(keyword) {
+  let prevSearch = JSON.parse(localStorage.getItem(SEARCH_KEY))
+  let searchArray = prevSearch
+
+  if (prevSearch === null) {
+    searchArray = []
+  }
+  searchArray.push(keyword)
+  localStorage.setItem(SEARCH_KEY, JSON.stringify(searchArray))
+}
 
 function inputHandler() {
   const keyword = searchInput.value
   // 최근 검색어 업데이트 하기
-  console.log(keyword)
   if (keyword === '') {
-    css(alert, 'display', 'block')
+    return css(alert, 'display', 'block')
   }
+  updateSearch(keyword)
 }
 
 function alertHandler() {
@@ -21,3 +33,8 @@ function alertHandler() {
 
 searchForm.addEventListener('submit', inputHandler)
 alertButton.addEventListener('click', alertHandler)
+
+// 최근 검색어 localStorage에 업데이트
+
+// let response = await tiger('http://localhost:3000/users')
+// console.log(response)
