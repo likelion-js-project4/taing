8e1fd4dc5d7a6482d0b5ab7abad778d4c13a2119
Yesong Choi <to06109@naver.com>
2023-02-07 00:37:49 +0900

    [검색] fix: 모두지우기 버튼 중복 실행 현상 해결

--- client/js/search.js
+++ client/js/search.js
@@ -1,11 +1,4 @@
-import {
-  getNode,
-  css,
-  tiger,
-  insertLast,
-  insertAfter,
-  clearContents,
-} from '../lib/index.js'
+import { getNode, css, insertAfter } from '../lib/index.js'
 import { renderSearchList } from '../js/render/renderSearchList.js'
 
 const SEARCH_KEY = 'taing_search'
@@ -20,9 +13,13 @@ const searchCurrentTitle = getNode('.search-current > h2')
 
 const deleteAllButton = getNode('.delete-all-btn')
 
+/* -------------------------------------------------------------------------- */
+/*                             최근검색어, 인기검색어 render                            */
+/* -------------------------------------------------------------------------- */
 function renderList() {
   let searchList = JSON.parse(localStorage.getItem(SEARCH_KEY))
-  if (!searchList) {
+  const noSearchKeyword = getNode('.search-current > p')
+  if (!searchList && !noSearchKeyword) {
     let template = /* html */ `<p>검색 내역이 없습니다.</p>`
     return insertAfter(searchCurrentTitle, template)
   }
@@ -33,6 +30,9 @@ function renderList() {
 }
 renderList()
 
+/* -------------------------------------------------------------------------- */
+/*                                 최근 검색어 업데이트                                */
+/* -------------------------------------------------------------------------- */
 function updateSearch(keyword) {
   let prevSearchArray = JSON.parse(localStorage.getItem(SEARCH_KEY))
   let searchArray = prevSearchArray
@@ -50,6 +50,9 @@ function updateSearch(keyword) {
   renderSearchList(searchCurrentTarget, keyword)
 }
 
+/* -------------------------------------------------------------------------- */
+/*                                 form 제출 이벤트                                */
+/* -------------------------------------------------------------------------- */
 function inputHandler() {
   const keyword = searchInput.value
   // 최근 검색어 업데이트 하기
@@ -64,6 +67,9 @@ function alertHandler() {
   css(alert, 'display', 'none')
 }
 
+/* -------------------------------------------------------------------------- */
+/*                                최근 검색어 모두 지우기                               */
+/* -------------------------------------------------------------------------- */
 // 노드의 자식노드 모두 삭제
 function removeChildAll(node) {
   while (node.firstChild) {
@@ -82,7 +88,10 @@ searchForm.addEventListener('submit', inputHandler)
 alertButton.addEventListener('click', alertHandler)
 deleteAllButton.addEventListener('click', clearSearch)
 
-// 최근 검색어 localStorage에 업데이트
-
 // let response = await tiger('http://localhost:3000/users')
 // console.log(response)
+
+function deleteHandler(e) {
+  console.log(e.target)
+}
+searchCurrentTarget.addEventListener('click', deleteHandler)
