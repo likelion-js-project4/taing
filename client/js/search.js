485facb1935000cad00cd6cc8f092169b6c48d01
Yesong Choi <to06109@naver.com>
2023-02-06 23:49:34 +0900

    [검색] feat: 최근검색어 렌더링 #20

--- client/js/search.js
+++ client/js/search.js
@@ -1,4 +1,5 @@
-import { getNode, css, tiger, saveStorage, loadStorage } from '../lib/index.js'
+import { getNode, css, tiger, insertLast, insertAfter } from '../lib/index.js'
+import { renderSearchList } from '../js/render/renderSearchList.js'
 
 const SEARCH_KEY = 'taing_search'
 
@@ -7,15 +8,37 @@ const searchForm = getNode('.search-form')
 const alert = getNode('.alert-section')
 const alertButton = getNode('.enroll-btn')
 
-function updateSearch(keyword) {
-  let prevSearch = JSON.parse(localStorage.getItem(SEARCH_KEY))
-  let searchArray = prevSearch
+const searchCurrentTarget = getNode('.search-current > ul')
+const searchCurrentTitle = getNode('.search-current > h2')
 
-  if (prevSearch === null) {
+function renderList() {
+  let searchList = JSON.parse(localStorage.getItem(SEARCH_KEY))
+  if (!searchList) {
+    let template = /* html */ `<p>검색 내역이 없습니다.</p>`
+    return insertAfter(searchCurrentTitle, template)
+  }
+  searchList.forEach((keyword) =>
+    renderSearchList(searchCurrentTarget, keyword),
+  )
+  // 인기 검색어 render
+}
+renderList()
+
+function updateSearch(keyword) {
+  let prevSearchArray = JSON.parse(localStorage.getItem(SEARCH_KEY))
+  let searchArray = prevSearchArray
+
+  if (prevSearchArray === null) {
     searchArray = []
   }
   searchArray.push(keyword)
   localStorage.setItem(SEARCH_KEY, JSON.stringify(searchArray))
+
+  // 검색 내역이 없습니다. 요소 지우기
+  const noSearchKeyword = getNode('.search-current > p')
+  if (noSearchKeyword) noSearchKeyword.remove()
+  // 업데이트된 최근검색어 렌더링
+  renderSearchList(searchCurrentTarget, keyword)
 }
 
 function inputHandler() {
