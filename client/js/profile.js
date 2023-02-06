58cf0c41c2c35f0ec72574d254589f2a99637c71
hwanghaein <hhi9037@naver.com>
2023-02-06 16:12:16 +0900

    [프로필] feat: 편집버튼, 완료버튼 바뀌는 토글 구현

--- client/js/profile.js
+++ client/js/profile.js
@@ -1,13 +1,31 @@
-import { getNode } from "../lib/index.js";
+import { getNode, removeClass, addClass} from "../lib/index.js";
 
-const profileEdit = getNode('.profile-edit-btn');
+const profileEditButton = getNode('#edit-btn');
 
-// console.log(profileEdit);
 
-function clickSubmitHandler(e) {
-  e.preventDefault();
-  console.log('클릭')
+let toggle = true;
+
+  //편집버튼을 누른상황 = true
+  // => 완료버튼이 나타나있는 상태
+
+  //완료버튼을 누른상황 = false
+  // => 편집버튼이 나타나있는 상태
+
+
+function clickEditHandler() {
+if (toggle) {
+removeClass('#edit-btn', 'profile-edit-btn')
+addClass('#edit-btn','profile-complete-btn')
+profileEditButton.textContent = "완료"
+toggle = !toggle;
+}else{
+removeClass('#edit-btn','profile-complete-btn')
+addClass('#edit-btn', 'profile-edit-btn')
+profileEditButton.textContent = "프로필 편집"
+  toggle = !toggle;
+}
 }
 
-profileEdit.addEventListener('click',clickSubmitHandler);
 
+
+profileEditButton.addEventListener('click',clickEditHandler);
