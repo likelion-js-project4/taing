8e6c1783efb2caa4d9cdb6b7c07120ba5b043e39
Yesong Choi <to06109@naver.com>
2023-02-06 18:03:50 +0900

    [검색] feat: 검색 입력값 없을 시 alert창 띄우기 #51

--- client/js/search.js
+++ client/js/search.js
@@ -1,12 +1,23 @@
-import { getNode } from '../lib/index.js'
+import { getNode, css } from '../lib/index.js'
 
 const searchInput = getNode('.search-form-input')
 const searchForm = getNode('.search-form')
+const alert = getNode('.alert-section')
+const alertButton = getNode('.enroll-btn')
 console.log(searchInput)
 
 function inputHandler() {
   const keyword = searchInput.value
+  // 최근 검색어 업데이트 하기
   console.log(keyword)
+  if (keyword === '') {
+    css(alert, 'display', 'block')
+  }
+}
+
+function alertHandler() {
+  css(alert, 'display', 'none')
 }
 
 searchForm.addEventListener('submit', inputHandler)
+alertButton.addEventListener('click', alertHandler)
