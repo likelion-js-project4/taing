5fbea7b5abe8b82b3035547d02e92b6b0d594ef4
semin99 <jsm7494@naver.com>
2023-02-07 10:10:40 +0900

    [랜딩] feat : horizontal scrolling js 구현 #84

--- client/js/landing.js
+++ client/js/landing.js
@@ -4,6 +4,29 @@ import {
   getNode as $,
 } from "../lib/index.js";
 
+let landingSwiper = new Swiper('.taing-only-contents .swiper', {
+  // direction: 'horizontal',
+  spaceBetween: 15,
+  centeredSlides: true,
+  breakpoints: {
+    320: {
+      slidesPerView: 1.2,
+      spaceBetween: 10,
+    },
+    500: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    1280: {
+      slidesPerView: 2.24,
+      spaceBetween: 30,
+    },
+  },
+})
+
 gsap.registerPlugin(ScrollTrigger);
 
 let sections = gsap.utils.toArray(".landing-img-box");
@@ -16,7 +39,22 @@ gsap.to(sections, {
     pin: true,
     scrub: 1,
     // snap: 1 / (sections.length - 1),
-    end: () => "+=" + $("#sectionPin").offsetWidth
+    end: () => "+=" + $("#sectionPin").offsetWidth,
+    onUpdate: ({progress})=>{
+      console.log(progress)
+      if(progress > 0.2){
+        landingSwiper.slideTo(0, 1000, true)
+      }
+      if(progress > 0.4){
+        landingSwiper.slideTo(1, 1000, true)
+      }
+      if(progress > 0.6){
+        landingSwiper.slideTo(2, 1000, true)
+      }
+      if(progress > 0.8){
+        landingSwiper.slideTo(3, 1000, true)
+      }
+    }
   }
 });
 
