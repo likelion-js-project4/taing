7c69ef3601712d978d22afe9ae1d2d2b39b0f29c
Yesong Choi <to06109@naver.com>
2023-02-07 17:00:18 +0900

    [랜딩페이지] fix: 슬라이더 배경 오류 수정

--- client/js/render/renderLanding.js
+++ client/js/render/renderLanding.js
@@ -1,32 +1,11 @@
 import { insertLast } from '../../lib/index.js'
 
-// 큰 이미지, 작은 이미지 둘 다 있을 때
-const createAllSwiper = ({ landing_swiper, landing_swiper_small, alt }) => {
+const createSwiperList = ({ landing_swiper, landing_swiper_small, alt }) => {
   return /* html */ `
     <div class="swiper-slide">
       <picture class="landing-img-box">
         <source media="(max-width: 1279px)" srcset=${landing_swiper_small}>
         <img src=${landing_swiper} alt=${alt} />
-      </picture>
-    </div>
-  `
-}
-
-const createLargeSwiper = ({ landing_swiper, alt }) => {
-  return /* html */ `
-    <div class="swiper-slide large-slide">
-      <picture class="landing-img-box ">
-        <img src=${landing_swiper} alt=${alt} />
-      </picture>
-    </div>
-  `
-}
-
-const createSmallSwiper = ({ landing_swiper_small, alt }) => {
-  return /* html */ `
-    <div class="swiper-slide small-slide">
-      <picture class="landing-img-box small-slide">
-        <img src=${landing_swiper_small} alt=${alt} />
       </picture>
     </div>
   `
@@ -43,13 +22,7 @@ const createRoller = ({ landing_roller, alt }) => {
 }
 
 export function renderLandingSwiper(target, data) {
-  const { landing_swiper, landing_swiper_small } = data
-  let template = ``
-  if (landing_swiper && landing_swiper_small) template = createAllSwiper(data)
-  else if (landing_swiper) template = createLargeSwiper(data)
-  else template = createSmallSwiper(data)
-
-  insertLast(target, template)
+  insertLast(target, createSwiperList(data))
 }
 
 export function renderRoller(target, data) {
