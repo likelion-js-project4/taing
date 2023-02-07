c4afbaa6ba07a6a2972566f1a86abd39f06acd44
semin99 <jsm7494@naver.com>
2023-02-08 01:06:06 +0900

    [메인] fix : autoPlayHandler 중복 삭제

--- client/js/createSwiper.js
+++ client/js/createSwiper.js
@@ -1,8 +1,8 @@
-import { getNode, insertFirst, addClass, removeClass, attr } from "../lib/index.js";
+import { getNode, insertFirst, addClass, removeClass, attr } from '../lib/index.js';
 
 export default function createSwiper() {
-  const visualSwiper = new Swiper(".visual .swiper", {
-    effect: "fade",
+  const visualSwiper = new Swiper('.visual .swiper', {
+    effect: 'fade',
 
     autoplay: {
       delay: 4000,
@@ -13,40 +13,38 @@ export default function createSwiper() {
     },
 
     navigation: {
-      nextEl: ".visual .swiper-button-next",
-      prevEl: ".visual .swiper-button-prev",
+      nextEl: '.visual .swiper-button-next',
+      prevEl: '.visual .swiper-button-prev',
     },
     pagination: {
-      el: ".visual .swiper-pagination",
+      el: '.visual .swiper-pagination',
       clickable: true,
     },
   });
 
-  const visualPagination = getNode(".visual .swiper-pagination-wrapper");
-  insertFirst(visualPagination, `<button type="button" class="swiper-button-autoplay" aria-label="멈춤"></button>`);
+  const visualPagination = getNode('.visual .swiper-pagination-wrapper');
+  insertFirst(visualPagination, `<button type='button' class='swiper-button-autoplay' aria-label='멈춤'></button>`);
 
-  const autoPlayButton = getNode(".swiper-button-autoplay");
+  const autoPlayButton = getNode('.swiper-button-autoplay');
 
-  let pause;
+  let pause = true;
 
   function autoPlayHandler() {
-    if (pause !== 1) {
+    if (pause) {
       visualSwiper.autoplay.stop();
-      addClass(autoPlayButton, "is-play");
-      attr(autoPlayButton, "aria-label", "재생");
-      pause = 1;
+      addClass(autoPlayButton, 'is-play');
+      attr(autoPlayButton, 'aria-label', '재생');
     } else {
       visualSwiper.autoplay.start();
-      removeClass(autoPlayButton, "is-play");
-      attr(autoPlayButton, "aria-label", "멈춤");
-      pause = 0;
+      removeClass(autoPlayButton, 'is-play');
+      attr(autoPlayButton, 'aria-label', '멈춤');
     }
+    pause = !pause;
   }
 
   autoPlayButton.addEventListener('click', autoPlayHandler);
 
-
-  const taingRecommendSwiper = new Swiper(".taing-recommend .swiper", {
+  const taingRecommendSwiper = new Swiper('.taing-recommend .swiper', {
     slidesPerView: 3,
     slidesPerGroup: 3,
     spaceBetween: 8,
@@ -62,16 +60,16 @@ export default function createSwiper() {
       },
     },
     navigation: {
-      nextEl: ".taing-recommend .swiper-button-next",
-      prevEl: ".taing-recommend .swiper-button-prev",
+      nextEl: '.taing-recommend .swiper-button-next',
+      prevEl: '.taing-recommend .swiper-button-prev',
     },
     pagination: {
-      el: ".taing-recommend .swiper-pagination",
+      el: '.taing-recommend .swiper-pagination',
       clickable: true,
     },
   });
 
-  const latestViewSwiper = new Swiper(".latest-view .swiper", {
+  const latestViewSwiper = new Swiper('.latest-view .swiper', {
     slidesPerView: 3,
     slidesPerGroup: 3,
     spaceBetween: 8,
@@ -87,16 +85,16 @@ export default function createSwiper() {
       },
     },
     navigation: {
-      nextEl: ".latest-view .swiper-button-next",
-      prevEl: ".latest-view .swiper-button-prev",
+      nextEl: '.latest-view .swiper-button-next',
+      prevEl: '.latest-view .swiper-button-prev',
     },
     pagination: {
-      el: ".latest-view .swiper-pagination",
+      el: '.latest-view .swiper-pagination',
       clickable: true,
     },
   });
 
-  const QuickVodSwiper = new Swiper(".quick-vod .swiper", {
+  const QuickVodSwiper = new Swiper('.quick-vod .swiper', {
     slidesPerView: 2,
     slidesPerGroup: 2,
     spaceBetween: 8,
@@ -112,16 +110,16 @@ export default function createSwiper() {
       },
     },
     navigation: {
-      nextEl: ".quick-vod .swiper-button-next",
-      prevEl: ".quick-vod .swiper-button-prev",
+      nextEl: '.quick-vod .swiper-button-next',
+      prevEl: '.quick-vod .swiper-button-prev',
     },
     pagination: {
-      el: ".quick-vod .swiper-pagination",
+      el: '.quick-vod .swiper-pagination',
       clickable: true,
     },
   });
 
-  const realTimeSwiper = new Swiper(".real-time .swiper", {
+  const realTimeSwiper = new Swiper('.real-time .swiper', {
     slidesPerView: 3,
     slidesPerGroup: 3,
     spaceBetween: 8,
@@ -137,16 +135,16 @@ export default function createSwiper() {
       },
     },
     navigation: {
-      nextEl: ".real-time .swiper-button-next",
-      prevEl: ".real-time .swiper-button-prev",
+      nextEl: '.real-time .swiper-button-next',
+      prevEl: '.real-time .swiper-button-prev',
     },
     pagination: {
-      el: ".real-time .swiper-pagination",
+      el: '.real-time .swiper-pagination',
       clickable: true,
     },
   });
 
-  const liveTimeSwiper = new Swiper(".live-time .swiper", {
+  const liveTimeSwiper = new Swiper('.live-time .swiper', {
     slidesPerView: 2,
     slidesPerGroup: 2,
     spaceBetween: 8,
@@ -162,16 +160,16 @@ export default function createSwiper() {
       },
     },
     navigation: {
-      nextEl: ".live-time .swiper-button-next",
-      prevEl: ".live-time .swiper-button-prev",
+      nextEl: '.live-time .swiper-button-next',
+      prevEl: '.live-time .swiper-button-prev',
     },
     pagination: {
-      el: ".live-time .swiper-pagination",
+      el: '.live-time .swiper-pagination',
       clickable: true,
     },
   });
 
-  const OnlyTaingSwiper = new Swiper(".only-taing .swiper", {
+  const OnlyTaingSwiper = new Swiper('.only-taing .swiper', {
     slidesPerView: 2,
     slidesPerGroup: 2,
     spaceBetween: 8,
@@ -187,16 +185,16 @@ export default function createSwiper() {
       },
     },
     navigation: {
-      nextEl: ".only-taing .swiper-button-next",
-      prevEl: ".only-taing .swiper-button-prev",
+      nextEl: '.only-taing .swiper-button-next',
+      prevEl: '.only-taing .swiper-button-prev',
     },
     pagination: {
-      el: ".only-taing .swiper-pagination",
+      el: '.only-taing .swiper-pagination',
       clickable: true,
     },
   });
 
-  const EventSwiper = new Swiper(".main-event .swiper", {
+  const EventSwiper = new Swiper('.main-event .swiper', {
     slidesPerView: 2,
     slidesPerGroup: 2,
     spaceBetween: 8,
@@ -213,28 +211,15 @@ export default function createSwiper() {
       },
     },
     navigation: {
-      nextEl: ".main-event .swiper-button-next",
-      prevEl: ".main-event .swiper-button-prev",
+      nextEl: '.main-event .swiper-button-next',
+      prevEl: '.main-event .swiper-button-prev',
     },
     pagination: {
-      el: ".main-event .swiper-pagination",
+      el: '.main-event .swiper-pagination',
       clickable: true,
     },
   });
 
-  function autoPlayHandler() {
-    if (pause !== 1) {
-      visualSwiper.autoplay.stop();
-      addClass(autoPlayButton, "is-play");
-      pause = 1;
-    } else {
-      visualSwiper.autoplay.start();
-      removeClass(autoPlayButton, "is-play");
-      pause = 0;
-    }
-  }
-
-  autoPlayButton.addEventListener("click", autoPlayHandler);
   return (
     visualSwiper,
     taingRecommendSwiper,
